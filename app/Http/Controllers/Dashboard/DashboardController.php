4a23b31133634746906f346a414866272ca2e2ea
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-02-23 16:03:05 +0500

    s

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -9,25 +9,25 @@ class DashboardController extends Controller
 {
     public function edit()
     {
-        return view('dashboard.pages.index'); // Ensure the view exists at resources/views/dashboard/profile/edit.blade.php
+        return view('dashboard.pages.index');
     }
     public static function login()
     {
-        return view('dashboard.auth.login'); // Ensure the view exists at resources/views/dashboard/auth/login.blade.php
+        return view('dashboard.auth.login');
     }
 
     public static function dashboard()
     {
-        return view('dashboard.pages.index'); // Ensure the view exists at resources/views/dashboard/pages/index.blade.php
+        return view('dashboard.pages.index');
     }
 
     public static function orders()
     {
-        return view('dashboard.orders.index'); // Ensure the view exists at resources/views/dashboard/orders/index.blade.php
+        return view('dashboard.orders.index');
     }
 
     public static function addProduct()
     {
-        return view('dashboard.products.add'); // Ensure the view exists at resources/views/dashboard/products/add.blade.php
+        return view('dashboard.products.add');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,21 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\frontend\FrontendController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\ProductController;
+use App\Http\Controllers\Dashboard\CategoryController;
 
 Route::get('/', [FrontendController::class, 'index'])->name('/');
 
-Route::get('/dashboard-1', [DashboardController::class, 'dashboard'])->name('dashboard');
 Route::get('/orders', [DashboardController::class, 'orders'])->name('orders');
 Route::get('/add-product', [DashboardController::class, 'addProduct'])->name('addProduct');
 
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('categories', CategoryController::class);
 });
 
 require __DIR__.'/auth.php';
